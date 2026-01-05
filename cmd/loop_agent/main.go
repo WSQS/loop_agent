02e56a8f836cddfa2f1c3f23a4f9622b67596de2
Sophomore <17792626+WSQS@users.noreply.github.com>
2026-01-05 17:01:14 +0800

    fix: enhance logging and directory creation for iflow command execution

--- cmd/loop_agent/main.go
+++ cmd/loop_agent/main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"bufio"
+	"io"
 	"log"
+	"os"
 	"os/exec"
+	"time"
 )
 
 func main() {
+	dir := ".loop_agent/" + time.Now().Format("060102150405")
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		log.Fatal(err)
+	}
+	f, err := os.OpenFile(dir+"/log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	log.SetOutput(io.MultiWriter(os.Stdout, f))
 	log.Println("hello, go module!")
 	cmd := exec.Command("iflow", "-y", "-d", "--thinking", "--prompt", "/init")
 	log.Println(cmd.String())
