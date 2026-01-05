d1df7c42e8c5aa8e33dfa5b54b01caa1a8f02013
Sophomore <17792626+WSQS@users.noreply.github.com>
2026-01-05 16:41:08 +0800

    fix: refactor command execution to use pipes for stdout and stderr

--- cmd/loop_agent/main.go
+++ cmd/loop_agent/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"bufio"
 	"log"
-	"os"
 	"os/exec"
 )
 
@@ -10,10 +10,33 @@ func main() {
 	log.Println("hello, go module!")
 	cmd := exec.Command("iflow", "-y", "-d", "--thinking", "--prompt", "/init")
 	log.Println(cmd.String())
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	if err := cmd.Run(); err != nil {
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
 		log.Fatal(err)
 	}
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		sc := bufio.NewScanner(stdout)
+		for sc.Scan() {
+			log.Println("[IFLOW-STDOUT]", sc.Text())
+		}
+	}()
+
+	go func() {
+		sc := bufio.NewScanner(stderr)
+		for sc.Scan() {
+			log.Println("[IFLOW-STDERR]", sc.Text())
+		}
+	}()
+
+	err = cmd.Wait()
 }
